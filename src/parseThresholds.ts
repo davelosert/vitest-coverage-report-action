be4fd9773a16f8904e1f5e725b7af129f34da0a8
David Losert <davelosert@github.com>
2022-06-17 08:34:25 +0000

    fix: Remove vite as dependency to read the thresholds to allow a standalone build

--- src/parseThresholds.ts
+++ src/parseThresholds.ts
@@ -1,35 +1,47 @@
 import path from 'node:path';
+import * as core from '@actions/core';
+import { promises as fs } from 'fs';
 import { Thresholds } from './types/Threshold';
 
-import { loadConfigFromFile } from 'vite';
+const regex100 = /100"?\s*:\s*true/;
+const regexStatements = /statements\s*:\s*(\d+)/;
+const regexLines = /lines:\s*(\d+)/;
+const regexBranches = /branches\s*:\s*(\d+)/;
+const regexFunctions = /functions\s*:\s*(\d+)/;
 
 const parseThresholds = async (vitestConfigPath: string): Promise<Thresholds> => {
-  const resolvedViteConfigPath = path.resolve(process.cwd(), vitestConfigPath);
-  const configObj = await loadConfigFromFile({command: 'build', mode: 'production' }, resolvedViteConfigPath);
+  try {
+    const resolvedViteConfigPath = path.resolve(process.cwd(), vitestConfigPath);
+    const rawContent = await fs.readFile(resolvedViteConfigPath, 'utf8');
 
-  if(!configObj) {
-    return {};
-  }
+    const has100Value = rawContent.match(regex100);
 
-  const config = configObj.config;
-
-  if(config.test?.coverage?.["100"]) {
-    return {
-      lines: 100,
-      branches: 100,
-      functions: 100,
-      statements: 100,
+    if(has100Value) {
+      return {
+        lines: 100,
+        branches: 100,
+        functions: 100,
+        statements: 100,
+      }
     }
-  }
-  
-  return {
-    lines: config.test?.coverage?.lines,
-    branches: config.test?.coverage?.branches,
-    functions: config.test?.coverage?.functions,
-    statements: config.test?.coverage?.statements,
+
+    const lines = rawContent.match(regexLines);
+    const branches = rawContent.match(regexBranches);
+    const functions = rawContent.match(regexFunctions);
+    const statements = rawContent.match(regexStatements);
+
+    return {
+      lines: lines ? parseInt(lines[1]) : undefined,
+      branches: branches ? parseInt(branches[1]) : undefined,
+      functions: functions ? parseInt(functions[1]) : undefined,
+      statements: statements ? parseInt(statements[1]) : undefined,
+    }
+  } catch (err) {
+    core.warning('Could not read vite config file for tresholds due to an error:', err);
+    return {};
   }
 }
 
 export {
-  parseThresholds
+  parseThresholds,
 };
