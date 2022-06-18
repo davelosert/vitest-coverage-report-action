e86480b0d75d010fe0e1517379e7f1a032844f6e
David Losert <davelosert@github.com>
2022-06-18 12:57:26 +0000

    ci: Adds pushing a short tag to release cycle

--- release.config.js
+++ release.config.js
@@ -3,13 +3,15 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/github', {
+    ['@semantic-release/github', {
         "assets": [
           {"path": "dist/index.js", "label": "Main Action File"},
         ]
       
-    }]
+  }],
+  ["@semantic-release/exec", {
+      "successCmd": "./moveTag.sh ${nextRelease.version}"
+  }],
   ]
 };
 
