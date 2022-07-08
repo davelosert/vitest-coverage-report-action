ecb44666199306436916c97ab1b23e60b83b17d5
David Losert <davelosert@github.com>
2022-07-08 13:46:30 +0000

    chore: Removes duplicated error handling

--- src/parseJsonReports.ts
+++ src/parseJsonReports.ts
@@ -3,6 +3,7 @@ import * as core from '@actions/core';
 import path from 'node:path';
 import { JsonFinal } from './types/JsonFinal';
 import { JsonSummary } from './types/JsonSummary';
+import { stripIndent } from 'common-tags';
 
 const parseJsonReport = async <type extends JsonSummary | JsonFinal>(jsonSummaryPath: string): Promise<type> => {
   const resolvedJsonSummaryPath = path.resolve(process.cwd(), jsonSummaryPath);
@@ -14,14 +15,13 @@ const parseJsonSummary = async (jsonSummaryPath: string): Promise<JsonSummary> =
   try {
     return await parseJsonReport<JsonSummary>(jsonSummaryPath);
   } catch (err: any) {
-    core.error(`
+    core.setFailed(stripIndent`
         Failed to parse the json-summary at path "${jsonSummaryPath}."
         Make sure to run vitest before this action and to include the "json-summay" reporter.
 
         Original Error:
         ${err.stack}
     `);
-    core.setFailed('coverage-summary.json not found - exiting.');
 
     // rethrow to abort everything
     throw err;
@@ -33,7 +33,7 @@ const parseJsonFinal = async (jsonFinalPath: string): Promise<JsonFinal> => {
   try {
     return await parseJsonReport<JsonFinal>(jsonFinalPath);
   } catch (err: any) {
-    core.warning(`
+    core.warning(stripIndent`
       Failed to parse JSON Final at path "${jsonFinalPath}".
       Line coverage will be empty. To include it, make sure to include the "json" reporter in your vitest execution.
 
