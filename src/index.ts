d968fba1fe87424da49d1195b6ad7de95c245cb5
David Losert <davelosert@github.com>
2022-07-08 10:37:14 +0000

    chore: Adds error-handling for json file parsing

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 import { generateSummaryTableData } from './generateSummaryTableHtml.js';
 import path from 'node:path';
-import { parseJsonReport } from './parseJsonReports.js';
+import { parseJsonFinal, parseJsonSummary } from './parseJsonReports.js';
 import { writeSummaryToPR } from './writeSummaryToPR.js';
 import * as core from '@actions/core';
 import { parseThresholds } from './parseThresholds.js';
-import { JsonSummary } from './types/JsonSummary.js';
-import { JsonFinal } from './types/JsonFinal.js';
 import { generateFileCoverageHtml } from './generateFileCoverageHtml.js';
 
 const run = async () => {
   const jsonSummaryPath = path.resolve(core.getInput('json-summary-path'));
   const jsonFinalPath = path.resolve(core.getInput('json-final-path'));
   const viteConfigPath = path.resolve(core.getInput('vite-config-path'));
-  const jsonSummary = await parseJsonReport<JsonSummary>(jsonSummaryPath);
-  const jsonFinal = await parseJsonReport<JsonFinal>(jsonFinalPath);
+
+  const jsonSummary = await parseJsonSummary(jsonSummaryPath);
+  const jsonFinal = await parseJsonFinal(jsonFinalPath);
   const thresholds = await parseThresholds(viteConfigPath);
 
   const tableData = generateSummaryTableData(jsonSummary.total, thresholds);
--- src/parseJsonReports.ts
+++ src/parseJsonReports.ts
@@ -1,4 +1,5 @@
 import { readFile } from 'node:fs/promises';
+import * as core from '@actions/core';
 import path from 'node:path';
 import { JsonFinal } from './types/JsonFinal';
 import { JsonSummary } from './types/JsonSummary';
@@ -9,6 +10,35 @@ const parseJsonReport = async <type extends JsonSummary | JsonFinal>(jsonSummary
   return JSON.parse(jsonSummaryRaw.toString()) as type;
 }
 
+const parseJsonSummary = async (jsonSummaryPath: string): Promise<JsonSummary> => {
+  try {
+    return parseJsonReport<JsonSummary>(jsonSummaryPath);
+  } catch (err) {
+    core.error(`
+        Failed to parse json summary at path "${jsonSummaryPath}."
+        Make sure to run vitest before this action an use the "json-summay" reporter.
+    `);
+    core.setFailed('coverage-summary.json not found - exiting.');
+
+    // rethrow to abort everything
+    throw err;
+
+  }
+}
+
+const parseJsonFinal = async (jsonFinalPath: string): Promise<JsonFinal> => {
+  try {
+    return parseJsonReport<JsonFinal>(jsonFinalPath);
+  } catch (err) {
+    core.warning(`
+      Failed to parse JSON Final at path "${jsonFinalPath}".
+      Line coverage will be empty. To include make sure to set the "json" reporter in your vitest configuration.
+    `);
+    return {};
+  }
+};
+
 export {
-  parseJsonReport
+  parseJsonSummary,
+  parseJsonFinal
 };
