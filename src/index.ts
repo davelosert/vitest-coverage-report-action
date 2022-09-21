ea0c3ecdc1e8d8dcd94c4d23bbae1c2761133907
Kendall Roth <kendall@kendallroth.ca>
2022-09-20 22:00:59 -0400

    Support custom working directory input

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,15 @@ import { parseThresholds } from './parseThresholds.js';
 import { generateFileCoverageHtml } from './generateFileCoverageHtml.js';
 
 const run = async () => {
-  const jsonSummaryPath = path.resolve(core.getInput('json-summary-path'));
-  const jsonFinalPath = path.resolve(core.getInput('json-final-path'));
-  const viteConfigPath = path.resolve(core.getInput('vite-config-path'));
+  // Working directory can be used to modify all default/provided paths (for monorepos, etc)
+  const workingDirectory = core.getInput('working-directory');
+  // TODO: 'path.resolve' will always result in an absolute URL, defaulting to using the current working
+  //         directory if no other absolute path was provided (from right to left). Therefore, joining
+  //         with 'process.cwd()' later has no effect (since the input will already be an absolute path).
+  //       Should likely clean up the unnecessary/duplicate 'path.resolve' usage where these variables are used?
+  const jsonSummaryPath = path.resolve(workingDirectory, core.getInput('json-summary-path'));
+  const jsonFinalPath = path.resolve(workingDirectory, core.getInput('json-final-path'));
+  const viteConfigPath = path.resolve(workingDirectory, core.getInput('vite-config-path'));
 
   const jsonSummary = await parseJsonSummary(jsonSummaryPath);
   const jsonFinal = await parseJsonFinal(jsonFinalPath);
