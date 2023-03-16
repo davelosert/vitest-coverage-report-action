8b0faf048f5e13849bab2bfd493de004210bcd79
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2023-03-16 10:08:01 +0100

    Added test suite for getViteConfigPath along with test config file.

---
+++ src/getViteConfigPath.test.ts
@@ -0,0 +1,32 @@
+import { describe, expect, it, vi } from "vitest";
+import path from "path";
+import * as core from "@actions/core"
+import { getViteConfigPath } from "./getViteConfigPath";
+
+describe("getViteConfigPath", () => {
+  const mockWorkingDirectory = path.resolve(
+    __dirname,
+    "..",
+    "test",
+    "mockConfig"
+  );
+
+  it("resolves with a full path if a file at the provided path exists", async (): Promise<void> => {
+    await expect(
+      getViteConfigPath(mockWorkingDirectory, "vitest.config.all.js")
+    ).resolves.toMatch('test/mockConfig/vitest.config.all.js');
+  });
+
+  it("resolves with a full path if no path is provided but a file with a default name exists", async (): Promise<void> => {
+    await expect(getViteConfigPath(mockWorkingDirectory, "")).resolves.toMatch(
+      'test/mockConfig/vitest.config.js'
+    );
+  });
+
+  it("rejects if config file can not be found", async (): Promise<void> => {
+    vi.spyOn(core, 'setFailed').mockImplementationOnce(() => { })
+    await expect(
+      getViteConfigPath(mockWorkingDirectory, "doesNotExist")
+    ).rejects.toThrow(/unable to find config file/i);
+  });
+});
